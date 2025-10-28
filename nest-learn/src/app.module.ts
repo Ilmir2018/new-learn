39df518e87ab7087fe6ad766afec64f3ac084047
ilmir.kiyametdinov <ilmir.kiyametdinov@mvideo.ru>
2025-10-28 19:38:42 +0300

    Подготовка проекта + создание необходимых модулей

--- nest-learn/src/app.module.ts
+++ nest-learn/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { TopPagesModule } from './top-pages/top-pages.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProductsModule, TopPagesModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
