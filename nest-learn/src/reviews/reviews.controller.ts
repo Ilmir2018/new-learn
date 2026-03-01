fe7a103660e16591e4e98a924fd65abf40a0189d
ilmir.kiyametdinov <ilmir.kiyametdinov@mvideo.ru>
2026-03-01 16:53:54 +0300

    создание кастомного декоратора и получение email из авторизационного токена

--- nest-learn/src/reviews/reviews.controller.ts
+++ nest-learn/src/reviews/reviews.controller.ts
@@ -13,6 +13,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewsService } from './reviews.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('reviews')
 export class ReviewsController {
@@ -39,8 +40,10 @@ export class ReviewsController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async get(@Param('productId') productId: string) {
+	async get(@Param('productId') productId: string, @UserEmail() email: string) {
+		console.log(email)
 		return this.reviewService.findByProductId(productId);
 	}
 }
