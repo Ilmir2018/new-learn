d97c52b5882ddbde60bfe501bb8e08922a6c204f
ilmir.kiyametdinov <ilmir.kiyametdinov@mvideo.ru>
2026-04-28 22:58:06 +0300

    конвертация изображений перед загрузкой

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { FileResponseElement } from './dto/file-element.responce';
 import { FilesService } from './files.service';
+import { MFile } from './mfile-class';
 
 @Controller('files')
 export class FilesController {
@@ -14,6 +15,14 @@ export class FilesController {
 	@UseGuards(JwtAuthGuard)
 	@UseInterceptors(FileInterceptor('file'))
 	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileResponseElement[]> {
-		return await this.filesService.uploadFile([file]);
+		const saveArray: MFile[] = [new MFile(file)];
+		if (file.mimetype.includes('image')) {
+			const buffer = await this.filesService.convertToWebP(file.buffer);
+			saveArray.push(new MFile({
+				originalname: `${file.originalname.split('.')[0]}.webp`,
+				buffer
+			}));
+		}
+		return await this.filesService.uploadFile(saveArray);
 	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,11 +3,13 @@ import { path } from 'app-root-path';
 import { format } from 'date-fns/format';
 import { ensureDir, writeFile } from 'fs-extra';
 import { FileResponseElement } from './dto/file-element.responce';
+import { MFile } from './mfile-class';
+import sharp from 'sharp';
 
 @Injectable()
 export class FilesService {
 
-	async uploadFile(files: Express.Multer.File[]): Promise<FileResponseElement[]> {
+	async uploadFile(files: MFile[]): Promise<FileResponseElement[]> {
 		const dateFolder = format(new Date(), 'yyyy-MM-dd');
 		const uploadFolder = `${path}/uploads/${dateFolder}`;
 		await ensureDir(uploadFolder);
@@ -20,5 +22,11 @@ export class FilesService {
 			});
 		}
 		return res;
+	}
+
+	convertToWebP(file: Buffer): Promise<Buffer> {
+		return sharp(file)
+			.webp()
+			.toBuffer();
 	}
 }
